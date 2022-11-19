321729bfbb02cab59732239d6f03992562c4b738
Silvan Wegmann <silvan.wegmann@hslu.ch>
2022-11-19 13:30:10 +0100

    Move IR code into a dedicated class

--- mybot/ir_remote.py
+++ mybot/ir_remote.py
@@ -26,33 +26,41 @@ BURST_LOOP_COUNTER = 30
 BIT_SAMPLE_DELAY = 15
 
 
-@rp2.asm_pio(in_shiftdir=rp2.PIO.SHIFT_RIGHT, autopush=True, push_thresh=32)
-def ir_receive(BURST_LOOP_COUNTER=BURST_LOOP_COUNTER, BIT_SAMPLE_DELAY=BIT_SAMPLE_DELAY):
-    wrap_target()
+class IRreceiver:
+    @rp2.asm_pio(in_shiftdir=rp2.PIO.SHIFT_RIGHT, autopush=True, push_thresh=32)
+    def ir_receive(BURST_LOOP_COUNTER=BURST_LOOP_COUNTER, BIT_SAMPLE_DELAY=BIT_SAMPLE_DELAY):
+        wrap_target()
 
-    label("next_burst")
-    set(x, BURST_LOOP_COUNTER)
-    wait(0, pin, 0)                     #  wait for the next burst to start
-    
-    label("burst_loop")
-    jmp(pin, "data_bit")                #  the burst ended before the counter expired
-    jmp(x_dec, "burst_loop")            #  wait for the burst to end
-    
-                                        #  the counter expired - this is a sync burst
-    mov(isr, null)                      #  reset the Input Shift Register
-    wait(1, pin, 0)                     #  wait for the sync burst to finish
-    jmp("next_burst")                   #  wait for the first data bit
-    
-    label("data_bit")
-    nop() [ BIT_SAMPLE_DELAY - 1 ]      #  wait for 1.5 burst periods before sampling the bit value
-    in_(pins, 1)                        #  if the next burst has started then detect a '0' (short gap)
-                                        #  otherwise detect a '1' (long gap)
-                                        #  after 32 bits the ISR will autop    
+        label("next_burst")
+        set(x, BURST_LOOP_COUNTER)
+        wait(0, pin, 0)                     #  wait for the next burst to start
+        
+        label("burst_loop")
+        jmp(pin, "data_bit")                #  the burst ended before the counter expired
+        jmp(x_dec, "burst_loop")            #  wait for the burst to end
+        
+                                            #  the counter expired - this is a sync burst
+        mov(isr, null)                      #  reset the Input Shift Register
+        wait(1, pin, 0)                     #  wait for the sync burst to finish
+        jmp("next_burst")                   #  wait for the first data bit
+        
+        label("data_bit")
+        nop() [ BIT_SAMPLE_DELAY - 1 ]      #  wait for 1.5 burst periods before sampling the bit value
+        in_(pins, 1)                        #  if the next burst has started then detect a '0' (short gap)
+                                            #  otherwise detect a '1' (long gap)
+                                            #  after 32 bits the ISR will autop    
 
-    wrap()
+        wrap()
 
+    def __init__(self):
+        ir = Pin(5, Pin.IN)
 
-ir = Pin(5, Pin.IN)
+        self.sm = rp2.StateMachine(0, IRreceiver.ir_receive, freq=int(10.0 / 562.5e-6), in_base=ir, jmp_pin=ir)
+        self.sm.active(1)
 
-sm = rp2.StateMachine(0, ir_receive, freq=int(10.0 / 562.5e-6), in_base=ir, jmp_pin=ir)
-sm.active(1)
+    def key_available(self):
+        return self.sm.rx_fifo() > 0
+
+    def get_key(self):
+        data = self.sm.get()
+        return (data & 0x00ff0000) >> 16
