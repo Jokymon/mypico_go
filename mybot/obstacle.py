c41bee4f0839cb821c3d2b9365d48760cc466e45
Silvan Wegmann <silvan.wegmann@hslu.ch>
2022-12-16 18:35:34 +0100

    Add ultrasonic ranging code

--- mybot/obstacle.py
+++ mybot/obstacle.py
@@ -1,4 +1,5 @@
 from machine import Pin
+import utime
 
 
 class IRobstacle:
@@ -14,3 +15,27 @@ class IRobstacle:
 
     def obstacle_front(self):
         return self.dsl.value() == 0 and self.dsr.value() == 0
+
+
+class Ultrasonic:
+    def __init__(self):
+        self.echo = Pin(15, Pin.IN)
+        self.trig = Pin(14, Pin.OUT)
+        self.echo.value(0)
+        self.trig.value(0)
+
+    def distance_cm(self):
+        self.trig.value(1)
+        utime.sleep_us(10)
+        self.trig.value(0)
+
+        while self.echo.value() == 0:
+            pass
+
+        t_start = utime.ticks_us()
+        while self.echo.value() == 1:
+            pass
+        t_end = utime.ticks_us()
+
+        distance = ((t_end - t_start)*0.034) / 2
+        return distance
