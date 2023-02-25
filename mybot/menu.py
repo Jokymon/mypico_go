0c6db617347849d3318c05c3302015496fb80f98
Silvan Wegmann <silvan.wegmann@hslu.ch>
2023-02-25 18:30:23 +0100

    Fix Python code formatting for PEP8

--- mybot/menu.py
+++ mybot/menu.py
@@ -25,23 +25,24 @@ class Menu:
         code = self.ir.get_key()
         if Keys.is_number(code):
             self.entered_code += str(Keys.NUMBER_KEYS.index(code))
-        if len(self.entered_code)==2:
+        if len(self.entered_code) == 2:
             selection = int(self.entered_code)
             self.entered_code = ""
-            if selection>=1 and selection<=len(self.page.entries):
+            if selection >= 1 and selection <= len(self.page.entries):
                 return self.page.entries[selection-1]["cb"]()
 
     def draw(self):
         self.lcd.fill(for_lcd(*Colors.BLACK))
         for index, entry in enumerate(self.page.entries):
-            self.lcd.text(f"{index+1:02} {entry['text']}", 
-                    Menu.MARGIN_LEFT, Menu.MARGIN_TOP + index*Menu.LINE_SPACING,
-                    for_lcd(*Colors.BLUE))
+            self.lcd.text(f"{index+1:02} {entry['text']}",
+                          Menu.MARGIN_LEFT,
+                          Menu.MARGIN_TOP + index*Menu.LINE_SPACING,
+                          for_lcd(*Colors.BLUE))
         self.lcd.text("Choose: ",
-                    100, Menu.MARGIN_TOP + 10*Menu.LINE_SPACING,
-                    for_lcd(*Colors.GREEN))
+                      100, Menu.MARGIN_TOP + 10*Menu.LINE_SPACING,
+                      for_lcd(*Colors.GREEN))
         self.lcd.text(self.entered_code+"_",
-                    180, Menu.MARGIN_TOP + 10*Menu.LINE_SPACING,
-                    for_lcd(*Colors.RED))
+                      180, Menu.MARGIN_TOP + 10*Menu.LINE_SPACING,
+                      for_lcd(*Colors.RED))
         self.page.draw(self.lcd)
         self.lcd.show()
